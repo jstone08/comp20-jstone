d4624699366dbfd7b0c0dacaef4e8cf951dcca17
jstone08 <jordanjamesstone@gmail.com>
2017-06-27 22:18:00 -0700

    update server address

--- mbta/mbta_stations.js
+++ mbta/mbta_stations.js
@@ -37,7 +37,7 @@ function popup() {
         }
     }
     
-    request.open("GET", "https://defense-in-derpth.herokuapp.com/redline.json", true);
+    request.open("GET", "https://sleepy-waters-78559.herokuapp.com/redline.json", true);
     request.send();
 }
 
